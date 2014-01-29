cae164de15ebeaef3fbef106c9199db02b6577c1
Benjamin Bittner <benjamin.bittner@omigos.de>
2014-01-29 11:20:45 +0100

    wrong class name

--- data/index.js
+++ data/index.js
@@ -6,7 +6,7 @@ var pattern = /^https:\/\/github.com\/(.*?)\/(.*?)\/(.*\d+)$/
 var m = href.match(pattern);
 div.setAttribute('data-account', JSON.stringify({'id':m[1]}));
 div.setAttribute('data-project', JSON.stringify({'id':m[2],'name':m[2]}));
-div.setAttribute('data-item', JSON.stringify({'id':m[3],'name':document.getElementsByClassName('js-topic-title')[0].innerHTML}));
+div.setAttribute('data-item', JSON.stringify({'id':m[3],'name':document.getElementsByClassName('js-issue-title')[0].innerHTML}));
 el.appendChild(div);
 var init = "window._harvestPlatformConfig = { 'applicationName': 'GitHub', 'permalink': 'https://github.com/%ACCOUNT_ID%/%PROJECT_ID%/%ITEM_ID%', 'referral': '66lx'}; var s = document.createElement('script');s.src = '//platform.harvestapp.com/assets/platform.js';s.async = true;var ph = document.getElementsByTagName('script')[0];ph.parentNode.insertBefore(s, ph);";
 var s = document.createElement('script');s.type = 'text/javascript';s.innerHTML = init;
