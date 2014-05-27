666f42899cd327d51a6d4c47d3f3f76680dd216e
MassiveHiggsField <benjamin.bittner@omigos.de>
2014-05-27 14:11:57 +0200

    #2 switched to a timer based solution

--- data/index.js
+++ data/index.js
@@ -1,28 +1,39 @@
-var href = window.location.href;
+if (window.location.href.match(/^https:\/\/github.com\/(.*?)\/(.*?)\/(.*\d+)$/))
+{
+    updateGitHub();
+    window.setInterval(function() { updateGitHub(); }, 3000);
+}
+else if (window.location.href.match(/^https:\/\/huboard.com\/.*$/))
+{
+    alert('huboard not integrated yet');
+}
 
-var pattern = /^https:\/\/github.com\/(.*?)\/(.*?)\/(.*\d+)$/;
-var m = href.match(pattern);
-var el = document.getElementsByClassName('gh-header-number')[0];
-var name = document.getElementsByClassName('js-issue-title')[0].innerHTML;
-el.appendChild(getTrackingElement(m[1], m[2], m[2], name));
-
-var ph = document.getElementsByTagName('script')[0];
-ph.parentNode.insertBefore(getScriptElement(), ph);
-
-function getScriptElement() {
+function updateGitHub() {
+    if (document.getElementsByClassName("harvest-timer").length == 0) {
+        addScriptElement();
+        var m = window.location.href.match(/^https:\/\/github.com\/(.*?)\/(.*?)\/(.*\d+)$/);
+        var name = document.getElementsByClassName('js-issue-title')[0].innerHTML;
+        document.getElementsByClassName('gh-header-number')[0].appendChild(
+            getTrackingElement(m[1], m[2], m[3], name)
+        );
+    }
+}
+function addScriptElement() {
     var s = document.createElement('script');
+    var ph = document.getElementsByTagName('script')[0];
+
     s.type = 'text/javascript';
     s.innerHTML = "window._harvestPlatformConfig = { " +
-        "'applicationName': 'GitHub', " +
-        "'permalink': 'https://github.com/%ACCOUNT_ID%/%PROJECT_ID%/%ITEM_ID%', " +
-        "'referral': '66lx'" +
-        "}; " +
-        "var s = document.createElement('script');" +
-        "s.src = '//platform.harvestapp.com/assets/platform.js';" +
-        "s.async = true;" +
-        "var ph = document.getElementsByTagName('script')[0];" +
-        "ph.parentNode.insertBefore(s, ph);";
-    return s;
+                    "'applicationName': 'GitHub', " +
+                    "'permalink': 'https://github.com/%ACCOUNT_ID%/%PROJECT_ID%/%ITEM_ID%', " +
+                    "'referral': '66lx'" +
+                "}; " +
+                "var s = document.createElement('script');" +
+                "s.src = '//platform.harvestapp.com/assets/platform.js';" +
+                "s.async = true;" +
+                "var ph = document.getElementsByTagName('script')[0];" +
+                "ph.parentNode.insertBefore(s, ph);";
+    ph.parentNode.insertBefore(s, ph);
 }
 function getTrackingElement(account, project, item, name) {
     var div = document.createElement('div');
