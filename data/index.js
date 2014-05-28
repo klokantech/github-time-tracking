32ab08bf711616e5c7b56ad018e70c427ea08ddf
MassiveHiggsField <benjamin.bittner@omigos.de>
2014-05-28 03:15:12 +0200

    #1 repositioned icon in huboard

--- data/index.js
+++ data/index.js
@@ -1,7 +1,5 @@
-if (document.getElementsByClassName("harvest-timer").length == 0) {
-    updatePage();
-    window.setInterval(function() { updatePage(); }, 3000);
-}
+updatePage();
+window.setInterval(function() { updatePage(); }, 3000);
 
 function updatePage() {
     if (document.getElementsByClassName("harvest-timer").length == 0) {
@@ -16,35 +14,22 @@ function updatePage() {
         }
     }
 }
-function appendHuboard() {
-    if (document.getElementsByClassName('fullscreen-header').length > 0) {
-        var basic = document.getElementsByClassName('fullscreen-header')[0].innerText.match(/^(.*) #(\d+)/);
-        var url = window.location.href.match(/^https:\/\/huboard.com\/(.*?)\/(.*?)#\/issues\/(.*\d+)$/);
-        var data = { 'account' : url[1], 'project' : url[2], 'id' : 'issues/' + basic[2], 'name' : basic[1] };
-        document.getElementsByClassName('fullscreen-header')[0].children[0].appendChild(createTrackingElement(data));
-    }
-}
-function appendGitHub() {
-    var url = window.location.href.match(/^https:\/\/github.com\/(.*?)\/(.*?)\/(.*\d+)$/);
-    var name = document.getElementsByClassName('js-issue-title')[0].innerHTML;
-    var data = { 'account' : url[1], 'project' : url[2], 'id' : url[3], 'name' : name };
-    document.getElementsByClassName('gh-header-number')[0].appendChild(createTrackingElement(data));
-}
+
 function addScriptElement() {
     var s = document.createElement('script');
     var ph = document.getElementsByTagName('script')[0];
 
     s.type = 'text/javascript';
     s.innerHTML = "window._harvestPlatformConfig = { " +
-                    "'applicationName': 'GitHub', " +
-                    "'permalink': 'https://github.com/%ACCOUNT_ID%/%PROJECT_ID%/%ITEM_ID%', " +
-                    "'referral': '66lx'" +
-                "}; " +
-                "var s = document.createElement('script');" +
-                "s.src = '//platform.harvestapp.com/assets/platform.js';" +
-                "s.async = true;" +
-                "var ph = document.getElementsByTagName('script')[0];" +
-                "ph.parentNode.insertBefore(s, ph);";
+        "'applicationName': 'GitHub', " +
+        "'permalink': 'https://github.com/%ACCOUNT_ID%/%PROJECT_ID%/%ITEM_ID%', " +
+        "'referral': '66lx'" +
+        "}; " +
+        "var s = document.createElement('script');" +
+        "s.src = '//platform.harvestapp.com/assets/platform.js';" +
+        "s.async = true;" +
+        "var ph = document.getElementsByTagName('script')[0];" +
+        "ph.parentNode.insertBefore(s, ph);";
     ph.parentNode.insertBefore(s, ph);
 }
 function createTrackingElement(data) {
@@ -54,4 +39,22 @@ function createTrackingElement(data) {
     div.setAttribute('data-project', JSON.stringify({ 'id' : data.project, 'name' : data.project }));
     div.setAttribute('data-item', JSON.stringify({ 'id' : data.id, 'name' : data.name }));
     return div;
+}
+
+function appendHuboard() {
+    if (document.getElementsByClassName('fullscreen-header').length > 0) {
+        var basic = document.getElementsByClassName('fullscreen-header')[0].innerText.match(/^(.*) #(\d+)/);
+        var url = window.location.href.match(/^https:\/\/huboard.com\/(.*?)\/(.*?)#\/issues\/(.*\d+)$/);
+        var data = { 'account' : url[1], 'project' : url[2], 'id' : 'issues/' + basic[2], 'name' : basic[1] };
+        var trackingElement = createTrackingElement(data);
+        trackingElement.style.cssFloat = 'right';
+        document.getElementsByClassName('fullscreen-header')[0].children[0].appendChild(trackingElement);
+    }
+}
+function appendGitHub() {
+    var url = window.location.href.match(/^https:\/\/github.com\/(.*?)\/(.*?)\/(.*\d+)$/);
+    var name = document.getElementsByClassName('js-issue-title')[0].innerHTML;
+    var data = { 'account' : url[1], 'project' : url[2], 'id' : url[3], 'name' : name };
+    var trackingElement = createTrackingElement(data);
+    document.getElementsByClassName('gh-header-number')[0].appendChild(trackingElement);
 }
